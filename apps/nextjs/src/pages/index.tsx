448269f3f3c5afcc279cf88ef6d604c0653efb05
Investments-Dao <info.investments-dao@proton.me>
2023-08-15 11:23:33 +0200

    15/8/23

--- apps/nextjs/src/pages/index.tsx
+++ apps/nextjs/src/pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <Head>
         <title>Inmo-Ocio</title>
         <meta name="description" content="Inmo-Ocio por equipo Investments-Dao.com" />
-        <link rel="icon" href="/favicon/favicon.ico" />
+        <link rel="icon" href="/favicon/icon.svg" />
       </Head>
       <Header />
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
