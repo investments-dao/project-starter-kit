f168d1c3d21c7ae1f3448008ac11534a551393d9
Investments-Dao <133033118+investments-dao@users.noreply.github.com>
2023-08-22 11:37:00 +0200

    Update signin.tsx

--- apps/nextjs/src/pages/auth/signin.tsx
+++ apps/nextjs/src/pages/auth/signin.tsx
@@ -8,15 +8,17 @@ type SigninProps = {};
 const Signin: NextPage = () => {
   const { t } = useTranslation(['nextjs']);
   return (
-    
-    <div className="mx-auto mt-16 max-w-sm">
-      <div className="relative flex flex-col items-center justify-center rounded-lg bg-slate-800 p-10">
-        <h3 className="mb-3 text-center text-2xl font-bold text-white sm:text-3xl">
+    <div>
+       <h3 className="mb-3 text-center text-2xl font-bold text-white sm:text-3xl">
                   Inicia sesion en tu cuenta
                 </h3>
                 <p className="mb-11 text-center text-base font-medium">
                   Logeate en tu cuenta.
                 </p>
+    </div>
+    <div className="mx-auto mt-16 max-w-sm">
+      <div className="relative flex flex-col items-center justify-center rounded-lg bg-slate-800 p-10">
+       
         <button
           className="bg-primary-200/20 text-primary-50 hover:bg-primary-200/30 relative flex min-w-fit items-center justify-center overflow-hidden whitespace-nowrap rounded-lg px-4 py-2 text-center text-sm font-semibold transition duration-100 ease-out"
           onClick={() => signIn('google')}
