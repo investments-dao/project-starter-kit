3c3e8bfea2195940f3c543f27daf77009b9d5941
Investments-Dao <133033118+investments-dao@users.noreply.github.com>
2023-08-22 10:46:21 +0200

    Update [...nextauth].ts

--- apps/nextjs/src/pages/api/auth/[...nextauth].ts
+++ apps/nextjs/src/pages/api/auth/[...nextauth].ts
@@ -1,4 +1,11 @@
 import NextAuth from 'next-auth';
 import { authOptions } from '@acme/auth';
-
+<div>
+ <h3 className="mb-3 text-center text-2xl font-bold text-black dark:text-white sm:text-3xl">
+                  Inicia sesion en tu cuenta
+                </h3>
+                <p className="mb-11 text-center text-base font-medium text-body-color">
+                  Logeate en tu cuenta.
+                </p>
+  </div>   
 export default NextAuth(authOptions);
