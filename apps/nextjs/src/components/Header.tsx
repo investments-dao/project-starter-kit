5b0d549ff4cb0b94fd785f31d3b45f8c946ae0b5
Investments-Dao <info.investments-dao@proton.me>
2023-08-28 10:34:14 +0200

    Update Header.tsx

--- apps/nextjs/src/components/Header.tsx
+++ apps/nextjs/src/components/Header.tsx
@@ -15,7 +15,7 @@ export const Header = () => {
       href="/">Inmo-Ocio</Link>
 
       <div className="dropdown">
-      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+      <ul>
         <li><a>Item 1</a></li>
         <li>
           <a>Parent</a>
