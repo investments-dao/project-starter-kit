1fe6cc5eb4ea9beeb9d9155d3a8cae905bf6cf4c
Investments-Dao <info.investments-dao@proton.me>
2023-08-28 11:49:49 +0200

    Update Header.tsx

--- apps/nextjs/src/components/Header.tsx
+++ apps/nextjs/src/components/Header.tsx
@@ -21,8 +21,7 @@ export const Header = () => {
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
       <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><Link className="btn btn-ghost normal-case text-xl"
-      href="/">Anuncia Gratis tu Inmueble</Link></li>
+        <li><a>Item 1</a></li>
         <li>
           <a>Parent</a>
           <ul className="p-2">
@@ -30,16 +29,14 @@ export const Header = () => {
             <li><a>Submenu 2</a></li>
           </ul>
         </li>
-        <li><Link className="btn btn-ghost normal-case text-xl"
-      href="/">Anuncia Gratis tu Actividad</Link></li>
+        <li><a>Item 3</a></li>
       </ul>
     </div>
     
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><<Link className="btn btn-ghost normal-case text-xl"
-      href="/">Anuncia Gratis tu Inmueble</Link></li>
+      <li><a>Item 1</a></li>
       <li tabIndex={0}>
         <details>
           <summary>Parent</summary>
@@ -49,8 +46,7 @@ export const Header = () => {
           </ul>
         </details>
       </li>
-      <li><Link className="btn btn-ghost normal-case text-xl"
-      href="/">Anuncia Gratis tu Actividad</Link></li>
+      <li><a>Item 3</a></li>
     </ul>
   </div>
 
