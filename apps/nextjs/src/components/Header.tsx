9e07b3110475991e501037ba19ae245de2c39bea
Investments-Dao <info.investments-dao@proton.me>
2023-08-28 11:22:38 +0200

    Update Header.tsx

--- apps/nextjs/src/components/Header.tsx
+++ apps/nextjs/src/components/Header.tsx
@@ -32,7 +32,7 @@ export const Header = () => {
         <li><a>Item 3</a></li>
       </ul>
     </div>
-    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+    
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
