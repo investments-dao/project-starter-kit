5686314196d17764107b915f6c1e2e6a5dbf8dd7
Investments-Dao <info.investments-dao@proton.me>
2023-08-28 12:14:52 +0200

    Update Header.tsx

--- apps/nextjs/src/components/Header.tsx
+++ apps/nextjs/src/components/Header.tsx
@@ -24,13 +24,7 @@ export const Header = () => {
         <li><Link href="/">
                   <p>Anuncias Gratis tu Inmueble</p>
                 </Link></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
+        
         <li><a>Item 3</a></li>
       </ul>
     </div>
